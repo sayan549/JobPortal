bbd6f18ce2f19b70b168b4b39c72ce9323832df0
sayan549 <sayantanbarman8@gmail.com>
2025-08-20 15:10:21 +0530

    Update api.ts

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
 });
 
 // Automatically attach token to every request
