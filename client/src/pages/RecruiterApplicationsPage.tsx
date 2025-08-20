b97ba4d342a7d273fb4561fbc26e7d558837e95f
sayan549 <sayantanbarman8@gmail.com>
2025-08-20 15:22:43 +0530

    Update RecruiterApplicationsPage.tsx

--- client/src/pages/RecruiterApplicationsPage.tsx
+++ client/src/pages/RecruiterApplicationsPage.tsx
@@ -48,7 +48,7 @@ export default function RecruiterApplicationsPage() {
       try {
         const token = localStorage.getItem("token");
         const { data } = await axios.get(
-          "http://jobportal-949c.onrender.com/api/v1/applications/recruiter",
+          "https://jobportal-949c.onrender.com/api/v1/applications/recruiter",
           {
             headers: { Authorization: `Bearer ${token}` },
           }
